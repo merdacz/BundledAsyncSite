a33bf01da3d961abc625a3fe5910783ff2389459
Marcin Daczkowski <merdacz@ais.pl>
2010-11-25 18:44:38 +0100

    Initial security context commit.

---
+++ BundledAsyncSite.Host/Security/BundledIdentity.cs
@@ -0,0 +1,23 @@
+﻿namespace BundledAsyncSite.Host.Security
+{
+    using System;
+    using System.Security.Principal;
+
+    public class BundledIdentity : IIdentity
+    {
+        public string Name
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string AuthenticationType
+        {
+            get { return "Custom"; }
+        }
+
+        public bool IsAuthenticated
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
---
+++ BundledAsyncSite.Host/Security/BundledPrincipal.cs
@@ -0,0 +1,20 @@
+﻿namespace BundledAsyncSite.Host.Security
+{
+    using System;
+    using System.Security.Principal;
+
+    public class BundledPrincipal : IPrincipal
+    {
+        public  BundledPrincipal(BundledIdentity identity)
+        {
+            this.Identity = identity;
+        }
+
+        public bool IsInRole(string role)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IIdentity Identity { get; private set; }
+    }
+}

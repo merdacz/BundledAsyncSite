74ebb6a49f61cf44c784867343be01aee536c31c
Marcin Daczkowski <merdacz@ais.pl>
2010-11-28 00:24:17 +0100

    SmtpClient is IDisposable and needs to be used inside using though.

--- BundledAsyncSite.Host/Bus/EventHandlers/SendEmailOnAccountCreated.cs
+++ BundledAsyncSite.Host/Bus/EventHandlers/SendEmailOnAccountCreated.cs
@@ -7,21 +7,23 @@
     {
         public override void Handle(AccountCreated @event)
         {
-            var client = new SmtpClient();
-            var from = new MailAddress("nobody@nowhere.com", "Nobody");
-            var to = new MailAddress(@event.Email, @event.UserName);
-            var message = new MailMessage(from, to);
-            message.Subject = "Your account has been created.";
-            message.Body = string.Format(
-@"
+            using (var client = new SmtpClient())
+            {
+                var from = new MailAddress("nobody@nowhere.com", "Nobody");
+                var to = new MailAddress(@event.Email, @event.UserName);
+                var message = new MailMessage(from, to);
+                message.Subject = "Your account has been created.";
+                message.Body = string.Format(
+                    @"
 Hello {0},
 you have been registered into our system. 
 
 Best regards,
 The Team
-", 
- @event.UserName);
-            client.Send(message);
+",
+                    @event.UserName);
+                client.Send(message);
+            }
         }
     }
 }
